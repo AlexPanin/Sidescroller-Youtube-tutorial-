da649bcee536326f57d901089ab79fb37360a890
Alex Panin <alexander.panin@eagles.ewu.edu>
2014-12-08 18:13:57 -0800

    Platform | Sidescroller 01
    Setup, X movement, and Ground/Head check.

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour
+{
+    // Player Handling
+	public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+	public float jumpHeight = 12;
+
+    private float currentSpeed;
+    private float targetSpeed;
+	private Vector2 amountToMove;
+
+	private PlayerPhysics playerPhysics;
+
+    void Start()
+    {
+		playerPhysics = GetComponent<PlayerPhysics>();
+    }
+
+    void Update()
+    {
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+		if (playerPhysics.grounded)
+		{
+			amountToMove.y = 0;
+			// Jump
+			if(Input.GetButtonDown("Jump"))
+			{
+				amountToMove.y = jumpHeight;
+			}
+		}
+
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove * Time.deltaTime);
+    }
+
+	// Increment speed towards target by acceleration
+	private float IncrementTowards(float s, float target, float acceleration)
+	{
+		if (s == target)
+			return s;
+		else
+		{
+			float dir = Mathf.Sign(target - s);									// Must speed be increased or decreased to get closer to target
+			s += acceleration * Time.deltaTime * dir;
+			return (dir == Mathf.Sign (target - s)) ? s : target;	// If speed has now passed target then return target, otherwise return speed
+		}
+	}
+}
---
+++ Platformer/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour
+{
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 size;
+	private Vector3 center;
+
+	private float skin = .005f;
+
+	public bool grounded;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start()
+	{
+		collider = GetComponent<BoxCollider>();
+		size = collider.size;
+		center = collider.center;
+	}
+
+	public void Move(Vector2 moveAmount)
+	{
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 position = transform.position;
+
+		grounded = false;
+
+		// Raycasting for bottom and top collision checks
+		for (int i = 0; i < 3; i++)
+		{
+			float dir = Mathf.Sign(deltaY);
+			float x = (position.x + center.x - size.x/2) + size.x/2 * i;	// Left, center and then rightmost point of collider
+			float y = position.y + center.y + size.y/2 * dir;				// Bottom of collider
+
+			ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+			Debug.DrawRay(ray.origin, ray.direction);
+			if (Physics.Raycast(ray, out hit, Mathf.Abs(deltaY), collisionMask))
+			{
+				// Get distance between player and ground
+				float distance = Vector3.Distance (ray.origin, hit.point);
+
+				// Stop player's downward movement after comming within skin width of a collider
+				if(distance > skin)
+					deltaY = -distance + skin;
+				else
+					deltaY = 0;
+
+				grounded = true;
+				break;
+			}
+		}
+
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+
+		transform.Translate(finalTransform);
+	}
+}
