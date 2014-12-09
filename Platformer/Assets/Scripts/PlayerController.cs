173630348d47f50068961b8f743d053146b29295
Alex Panin <alexander.panin@eagles.ewu.edu>
2014-12-08 20:36:13 -0800

    Platform | Sidescroller 03
    Finishing up the controller, import player, and set up some animations

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -6,33 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     // Player Handling
 	public float gravity = 20;
-    public float speed = 8;
+    public float walkSpeed = 8;
+	public float runSpeed = 12;
     public float acceleration = 30;
 	public float jumpHeight = 12;
 
+	private float animationSpeed;
     private float currentSpeed;
     private float targetSpeed;
 	private Vector2 amountToMove;
 
 	private PlayerPhysics playerPhysics;
+	private Animator animator;
 
     void Start()
     {
 		playerPhysics = GetComponent<PlayerPhysics>();
+		animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+		// Reset acceleration upon collision
 		if (playerPhysics.movementStopped)
 		{
 			targetSpeed = 0;
 			currentSpeed = 0;
 		}
-
-		// Input
-		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
-		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
-
+		
+		// If player is touching the ground
 		if (playerPhysics.grounded)
 		{
 			amountToMove.y = 0;
@@ -43,9 +45,23 @@ public class PlayerController : MonoBehaviour
 			}
 		}
 
+		animationSpeed = IncrementTowards(animationSpeed, Mathf.Abs(targetSpeed), acceleration);
+		animator.SetFloat("Speed", animationSpeed);
+
+		// Input
+		float speed = (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+		// Set amount to move
 		amountToMove.x = currentSpeed;
 		amountToMove.y -= gravity * Time.deltaTime;
 		playerPhysics.Move(amountToMove * Time.deltaTime);
+
+		// Face in the right direction
+		float moveDir = Input.GetAxisRaw("Horizontal");
+		if (moveDir != 0)
+			transform.eulerAngles = (moveDir > 0) ? Vector3.up * 180 : Vector3.zero;
     }
 
 	// Increment speed towards target by acceleration
--- Platformer/Assets/Scripts/PlayerPhysics.cs
+++ Platformer/Assets/Scripts/PlayerPhysics.cs
@@ -7,8 +7,15 @@ public class PlayerPhysics : MonoBehaviour
 	public LayerMask collisionMask;
 
 	private BoxCollider collider;
-	private Vector3 size;
-	private Vector3 center;
+	private Vector3 s;
+	private Vector3 c;
+
+	private Vector3 originalSize;
+	private Vector3 originalCenter;
+	private float colliderScale;
+
+	private int collisionDivisionX = 3;
+	private int collisionDivisionY = 10;
 
 	private float skin = .005f;
 
@@ -23,8 +30,11 @@ public class PlayerPhysics : MonoBehaviour
 	void Start()
 	{
 		collider = GetComponent<BoxCollider>();
-		size = collider.size;
-		center = collider.center;
+		colliderScale = transform.localScale.x;
+
+		originalSize = collider.size;
+		originalCenter = collider.center;
+		SetCollider(originalSize, originalCenter);
 	}
 
 	public void Move(Vector2 moveAmount)
@@ -35,11 +45,11 @@ public class PlayerPhysics : MonoBehaviour
 
 		// Check collisions above and below
 		grounded = false;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < collisionDivisionX; i++)
 		{
 			float dir = Mathf.Sign(deltaY);
-			float x = (position.x + center.x - size.x/2) + size.x/2 * i;	// Left, center and then rightmost point of collider
-			float y = position.y + center.y + size.y/2 * dir;				// Bottom of collider
+			float x = (position.x + c.x - s.x/2) + s.x/(collisionDivisionX - 1) * i;	// Left, center and then rightmost point of collider
+			float y = position.y + c.y + s.y/2 * dir;				// Bottom of collider
 
 			ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
 			Debug.DrawRay(ray.origin, ray.direction);
@@ -61,11 +71,11 @@ public class PlayerPhysics : MonoBehaviour
 
 		// Check collisions left and right
 		movementStopped = false;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < collisionDivisionY; i++)
 		{
 			float dir = Mathf.Sign(deltaX);
-			float x = position.x + center.x + size.x/2 * dir;	// Left, center and then rightmost point of collider
-			float y = position.y + center.y - size.y/2 + size.y/2 * i;				// Bottom of collider
+			float x = position.x + c.x + s.x/2 * dir;	// Left, center and then rightmost point of collider
+			float y = position.y + c.y - s.y/2 + s.y/(collisionDivisionY - 1) * i;				// Bottom of collider
 			
 			ray = new Ray(new Vector2(x, y), new Vector2(dir, 0));
 			Debug.DrawRay(ray.origin, ray.direction);
@@ -85,8 +95,32 @@ public class PlayerPhysics : MonoBehaviour
 			}
 		}
 
+		// Check collisions in the direction of movement
+		if (!grounded && !movementStopped)
+		{
+			Vector3 playerDir = new Vector3(deltaX, deltaY);
+			Vector3 o = new Vector3(position.x + c.x + s.x / 2 * Mathf.Sign(deltaX), 
+		                        	position.y + c.y + s.y / 2 * Mathf.Sign(deltaY));
+			ray = new Ray(o, playerDir.normalized);
+			Debug.DrawRay(ray.origin, ray.direction);
+			if (Physics.Raycast(ray, Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY), collisionMask))
+			{
+				grounded = true;
+				deltaY = 0;
+			}
+		}
+
 		Vector2 finalTransform = new Vector2(deltaX, deltaY);
 
-		transform.Translate(finalTransform);
+		transform.Translate(finalTransform, Space.World);
+	}
+
+	public void SetCollider(Vector3 size, Vector3 center)
+	{
+		collider.size = size;
+		collider.center = center;
+
+		s = size * colliderScale;
+		c = center * colliderScale;
 	}
 }
